36eb3136e1fe853d2188b9ebb670f61832ac88d2
SoinSoin <vincent.retore@gmail.com>
2018-03-05 09:14:50 +0100

    correction de bug js

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -1,27 +1,69 @@
-$(document).ready(function test() {
+$(document).ready(function () {
     $(document).alton({
         firstClass: 'header', // Set the first container class
         scrollMode: 'headerScroll', // Set the scroll mode
     });
-var ordi=window.matchMedia("(max-width:768px)");
-var tel = window.matchMedia("(max-device-width:481px)");
-    if (tel.matches || ordi.matches){
-        console.log("boucle");
-        var forEach = function (t, o, r) {
-            if ("[object Object]" === Object.prototype.toString.call(t))
-                for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
-            else
-                for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
-        };
+    $("#fotome").on("click", function () {
+        window.location.replace("moi.php");
+    });
+    
+    $('.titre,#fléche').on('click', function () { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 850; // Durée de l'animation (en ms)
+        $('html, body').animate({
+            scrollTop: $(page).offset().top
+        }, speed); // Go
+        return false;
+    });
+    
+    $("#html").on("click", function () {
+        $("#css").removeClass("test");
+        $("#textcss").hide(800);
+        $("#html").toggleClass("test");
+        $("#texthtml").toggle(800);
+        console.log("passe html");
+    });
 
-        var hamburgers = document.querySelectorAll(".hamburger");
-        if (hamburgers.length > 0) {
-            forEach(hamburgers, function (hamburger) {
-                hamburger.addEventListener("click", function () {
-                    this.classList.toggle("is-active");
-                }, false);
-            });
-        }
+    $("#css").on("click", function () {
+        $("#html").removeClass("test");
+        $("#texthtml").hide(800);
+        $("#css").toggleClass("test");
+        $("#textcss").toggle(800);
+        console.log("passe html");
+    });
+
+
+
+
+
+
+
+
+    var forEach = function (t, o, r) {
+        if ("[object Object]" === Object.prototype.toString.call(t))
+            for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+        else
+            for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
+    };
+
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+        forEach(hamburgers, function (hamburger) {
+            hamburger.addEventListener("click", function () {
+                this.classList.toggle("is-active");
+            }, false);
+        });
+    }
+});
+
+
+
+
+
+$(document).ready(function test() {
+    var ordi = window.matchMedia("(max-width:768px)");
+    var tel = window.matchMedia("(max-device-width:481px)");
+    if (tel.matches || ordi.matches) {
         $('#containermenu').css({
             display: 'none'
         });
@@ -41,7 +83,7 @@ var tel = window.matchMedia("(max-device-width:481px)");
 
         }
         var bouttonBurg = $(".hamburger");
-        $(".hamburger").on("click",function () {
+        $(".hamburger").on("click", function () {
             if (click == false && bouttonBurg.hasClass("is-active")) {
                 $("#macdo").addClass("is-active");
             } else if (click == true) {
@@ -57,13 +99,5 @@ var tel = window.matchMedia("(max-device-width:481px)");
         });
     }
     window.addEventListener('resize', test, true);
-
-    $('.titre,#fléche').on('click', function() { // Au clic sur un élément
-        var page = $(this).attr('href'); // Page cible
-        var speed = 750; // Durée de l'animation (en ms)
-        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-        return false;
-    });
-
-
 });
+
---
+++ app/index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Vincent Retore</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="dist/css/Style.css">
+	<link rel="stylesheet" href="dist/css/resp.css">
+	<link rel="stylesheet" media="(max-device-width: 481px)" href="dist/css/tel.css">
+
+
+
+</head>
+
+<body>
+	<div id="nuage">
+		<img src="dist/img/nuage/nuages-2.png" id="nuage1">
+		<img src="dist/img/nuage/nuages-1.png" id="nuage2">
+		<img src="dist/img/nuage/nuages-3.png" id="nuage3">
+	</div>
+
+
+	</div>
+	<header>
+		<div id="barnav">
+			<div id="moi">
+				<img src="dist/img/Igypop.jpg" id="fotome" title="Vincent RETORE">
+			</div>
+			<div id="burger">
+				<div id="macdo" class="hamburger hamburger--spring-r">
+					<div class="hamburger-box">
+						<div class="hamburger-inner"></div>
+					</div>
+				</div>
+			</div>
+			<div id=containermenu>
+				<div id="techno" class="menu">
+					<a class="titre" href="#technologie">Technologies</a>
+				</div>
+				<div id="comp" class="menu">
+					<a class="titre" href="#competence">Compétences</a>
+				</div>
+				<div id="formation" class="menu">
+					<a class="titre" href="#forma">Formation</a>
+				</div>
+				<div id="projet" class="menu">
+					<a class="titre" href="#prj">Projets</a>
+				</div>
+				<div id="expe" class="menu">
+					<a class="titre" href="#experience">Expériences</a>
+				</div>
+			</div>
+		</div>
+
+	</header>
+
+
+	<section class="header">
+		<div id="filter">
+			<div id="elementfiltre">
+				<div id="intitulé">
+					<h1>
+						Développeur Web
+						<br> Junior
+					</h1>
+				</div>
+				<a id="fléche" href="#technologie">
+					<img src="dist/img/down-arrow.png" id="arrowbot">
+				</a>
+			</div>
+		</div>
+	</section>
+
+	<section class="pageWrapper">
+		<div id="containerinfo">
+			<div class="autourinfo"id="technologie">
+				<div  class="info">
+					<div class="titreinfo" id="titretechno">
+						<h2>
+							Technologies
+						</h2>
+					</div>
+					<div id="autour">
+						<div class="bouton" id="html">
+							bouton
+						</div>
+						<div class="bouton" id="css">
+							<a>bouton</a>
+						</div>
+						<div class="bouton" id="css">
+							<a>bouton</a>
+						</div>
+						<div class="bouton" id="css">
+							<a>bouton</a>
+						</div>
+						<div class="bouton" id="css">
+							<a>bouton</a>
+						</div>
+					</div>
+					<div id="texthtml">
+						<p class="texte">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam assumenda minus id nam totam tenetur est laboriosam
+							facilis sapiente suscipit perspiciatis voluptatibus omnis aliquid culpa amet illum in, quidem dicta tempore! Facere
+							temporibus voluptates earum. Consectetur dolorum earum laborum ratione iste, eligendi ab nulla obcaecati quam esse
+							quae illo temporibus sequi quia vitae ducimus repudiandae quibusdam tempore minima cupiditate fuga consequuntur velit
+							nam unde! Accusamus maxime, doloremque amet tenetur aliquid dolorum rem, autem nisi minima eveniet nam? Neque eveniet
+							fuga culpa praesentium maxime aut a quae ullam, delectus accusamus quidem esse quia eius, harum officia deleniti animi
+							autem nobis?
+						</p>
+					</div>
+					<div id="textcss">
+						<p class="texte">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam assumenda minus id nam totam tenetur est laboriosam
+							facilis sapiente suscipit perspiciatis voluptatibus omnis aliquid culpa amet illum in, quidem dicta tempore! Facere
+							temporibus voluptates earum. Consectetur dolorum earum laborum ratione iste, eligendi ab nulla obcaecati quam esse
+							quae illo temporibus sequi quia vitae ducimus repudiandae quibusdam tempore minima cupiditate fuga consequuntur velit
+							nam unde! Accusamus maxime, doloremque amet tenetur aliquid dolorum rem, autem nisi minima eveniet nam? Neque eveniet
+							fuga culpa praesentium maxime aut a quae ullam, delectus accusamus quidem esse quia eius, harum officia deleniti animi
+							autem nobis?
+						</p>
+					</div>
+
+				</div>
+			</div>
+			<div class="autourinfo" id="competence">
+				<div  class="info">
+					<div class="titreinfo">
+						<h2>
+							Lorem ipsum
+						</h2>
+					</div>
+				</div>
+			</div>
+			<div class="autourinfo"id="forma">
+				<div  class="info">
+					<div class="titreinfo">
+						<h2>
+							Lorem ipsum
+						</h2>
+					</div>
+				</div>
+			</div>
+			<div class="autourinfo" id="prj">
+				<div  class="info">
+					<div class="titreinfo">
+						<h2>
+							Lorem ipsum
+						</h2>
+					</div>
+				</div>
+			</div>
+			<div class="autourinfo" id="experience">
+				<div  class="info">
+					<div class="titreinfo">
+						<h2>
+							Lorem ipsum
+						</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+	</section>
+
+	<footer>
+
+	</footer>
+
+	<script src="dist/js/jquery.min.js"></script>
+	<script src="dist/js/jquery.alton.min.js"></script>
+	<script src="dist/js/app.js"></script>
+</body>
+
+</html>
