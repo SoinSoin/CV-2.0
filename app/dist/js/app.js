39d436cc3bb8fe2cb27748a99f6a2a018009ac02
SoinSoin <vincent.retore@gmail.com>
2018-03-18 14:18:06 +0100

    la fin est proche

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
-    function add() {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        center:true,
+        items:2,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive: {
+            0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:2,
+        }
+    }
+    });
+
+
         $('#technologie').bind('inview', function (event, isInView) {
             if (isInView) {
                 $('#titretechno').addClass('couleurbas');
@@ -38,7 +61,6 @@ $(document).ready(function () {
                 $('#titreexpe').removeClass("couleurbas");
             }
         });
-    }
     
     $("#fotome").on("click", function () {
         window.location.replace("moi.php");
@@ -57,33 +79,33 @@ $(document).ready(function () {
         $("#css,#js,#php,#sql").removeClass("test");
         $("#textcss,#textjs,#textphp,#textsql").hide(800);
         $("#html").toggleClass("test");
-        $("#texthtml").toggle(800).css("display", "flex");
+        $("#texthtml").animate({width:'toggle'},800).css("display", "flex");
     });
 
     $("#css").on(" click", function () {
         $("#html,#js,#php,#sql").removeClass("test");
         $("#texthtml,#textjs,#textphp,#textsql").hide(800);
         $("#css").toggleClass("test");
-        $("#textcss").toggle(800).css("display", "flex");
+        $("#textcss").animate({width:'toggle'},800).css("display", "flex");
     });
 
     $("#js").on(" click", function () {
         $("#html,#css,#php,#sql").removeClass("test");
         $("#texthtml,#textphp,#textsql,#textcss").hide(800);
         $("#js").toggleClass("test");
-        $("#textjs").toggle(800).css("display", "flex");
+        $("#textjs").animate({width:'toggle'},800).css("display", "flex");
     });
     $("#php").on(" click", function () {
         $("#html,#css,#js,#sql").removeClass("test");
         $("#texthtml,#textjs,#textsql,#textcss").hide(800);
         $("#php").toggleClass("test");
-        $("#textphp").toggle(800).css("display", "flex");
+        $("#textphp").animate({width:'toggle'},800).css("display", "flex");
     });
     $("#sql").on(" click", function () {
         $("#html,#css,#php,#js").removeClass("test");
         $("#texthtml,#textphp,#textjs,#textcss").hide(800);
         $("#sql").toggleClass("test");
-        $("#textsql").toggle(800).css("display", "flex");
+        $("#textsql").animate({width:'toggle'},800).css("display", "flex");
     });
     var forEach = function (t, o, r) {
         if ("[object Object]" === Object.prototype.toString.call(t))
@@ -99,102 +121,106 @@ $(document).ready(function () {
             }, false);
         });
     }
-    $(window).resize(function () {
+
+    function respTuile() {
         var ordi = window.matchMedia("(max-width:768px)");
         var laptop = window.matchMedia("(max-width:1280px)");
         if (laptop.matches || ordi.matches) {
-            add();
             $(".imgsav").css("display", "block");
             $(".fiche").css("display", "none");
             var click = false;
+            var click1 = false;
+            var click2 = false;
+            var click3 = false;
+            var click4 = false;
+            var click5 = false;
             $("#comp1").click(function () {
                 if (click) {
-                    $("#imgcomp1,#imgsav1").show(800);
-                    $("#fiche1").hide(800);
+                    $("#imgcomp1,#imgsav1").show();
+                    $("#fiche1").hide();
 
                     click = false;
                 } else {
-                    $("#imgcomp1,#imgsav1").hide(800);
-                    $("#fiche1").show(800);
+                    $("#imgcomp1,#imgsav1").hide();
+                    $("#fiche1").show();
                     click = true;
                 }
             });
             $("#comp2").click(function () {
-                if (click) {
-                    $("#imgcomp2,#imgsav2").show(800);
-                    $("#fiche2").hide(800);
+                if (click1) {
+                    $("#imgcomp2,#imgsav2").show();
+                    $("#fiche2").hide();
 
-                    click = false;
+                    click1 = false;
                 } else {
-                    $("#imgcomp2,#imgsav2").hide(800);
-                    $("#fiche2").show(800);
-                    click = true;
+                    $("#imgcomp2,#imgsav2").hide();
+                    $("#fiche2").show();
+                    click1 = true;
                 }
             });
             $("#comp3").click(function () {
-                if (click) {
-                    $("#imgcomp3,#imgsav3").show(800);
-                    $("#fiche3").hide(800);
+                if (click2) {
+                    $("#imgcomp3,#imgsav3").show();
+                    $("#fiche3").hide();
 
-                    click = false;
+                    click2 = false;
                 } else {
-                    $("#imgcomp3,#imgsav3").hide(800);
-                    $("#fiche3").show(800);
-                    click = true;
+                    $("#imgcomp3,#imgsav3").hide();
+                    $("#fiche3").show();
+                    click2 = true;
                 }
             });
             $("#comp4").click(function () {
-                if (click) {
-                    $("#imgcomp4,#imgsav4").show(800);
-                    $("#fiche4").hide(800);
+                if (click3) {
+                    $("#imgcomp4,#imgsav4").show();
+                    $("#fiche4").hide();
 
-                    click = false;
+                    click3 = false;
                 } else {
-                    $("#imgcomp4,#imgsav4").hide(800);
-                    $("#fiche4").show(800);
-                    click = true;
+                    $("#imgcomp4,#imgsav4").hide();
+                    $("#fiche4").show();
+                    click3 = true;
                 }
             });
             $("#comp5").click(function () {
-                if (click) {
-                    $("#imgcomp5,#imgsav5").show(800);
-                    $("#fiche5").hide(800);
+                if (click4) {
+                    $("#imgcomp5,#imgsav5").show();
+                    $("#fiche5").hide();
 
-                    click = false;
+                    click4 = false;
                 } else {
-                    $("#imgcomp5,#imgsav5").hide(800);
-                    $("#fiche5").show(800);
-                    click = true;
+                    $("#imgcomp5,#imgsav5").hide();
+                    $("#fiche5").show();
+                    click4 = true;
                 }
             });
             $("#comp6").click(function () {
-                if (click) {
-                    $("#imgcomp6,#imgsav6").show(800);
-                    $("#fiche6").hide(800);
+                if (click5) {
+                    $("#imgcomp6,#imgsav6").show();
+                    $("#fiche6").hide();
 
-                    click = false;
+                    click5 = false;
                 } else {
-                    $("#imgcomp6,#imgsav6").hide(800);
-                    $("#fiche6").show(800);
-                    click = true;
+                    $("#imgcomp6,#imgsav6").hide();
+                    $("#fiche6").show();
+                    click5 = true;
                 }
             });
 
         } else {
+
             $(".comp").unbind("click");
             $(".imgsav").hide();
             $(".imgcomp").show();
             $(".fiche").css("display", "flex");
         }
-    });
-
-    
-    $(window).resize(function () {
-        // add(false);
+        window.addEventListener('resize', respTuile, true);
+    }
+    respTuile()
+    function menuResp() {
         var ordi = window.matchMedia("(max-width:768px)");
         var tel = window.matchMedia("(max-device-width:481px)");
         if (tel.matches || ordi.matches) {
-
             $('#containermenu').css({
                 display: 'none'
             });
@@ -223,14 +249,13 @@ $(document).ready(function () {
                 event();
             });
 
-
         } else {
             $("#macdo").removeClass("is-active");
             $('#containermenu').css({
                 display: 'flex'
             });
-            add();
         }
-
-    });
+        window.addEventListener('resize', menuResp, true);
+    }
+    menuResp()
 });
